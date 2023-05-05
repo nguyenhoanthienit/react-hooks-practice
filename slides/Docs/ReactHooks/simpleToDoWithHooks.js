ca7885ff624459cd9e12124d4ded90aa800d3aaa
ThienHNguyenBillIdentity <thien.nguyen@optimatech.io>
2023-05-05 21:16:02 +0700

    add docs

---
+++ slides/Docs/ReactHooks/simpleToDoWithHooks.js
@@ -0,0 +1,45 @@
+// index.js
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() === "") {
+      return;
+    }
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+    setNewTodo("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
